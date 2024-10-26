7d99742e6aa248f346cc0a3d540fab872c66355d
Utkarsh Prakash Joshilkar <utkarshjoshilkar@icloud.com>
2024-10-26 23:04:26 +0530

    Time: 1 ms (99.82%), Space: 45.8 MB (70.19%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,24 +1,37 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-        for(int i=0;i<n;i++){
-            int value = mpp.getOrDefault(nums[i],0);
-            mpp.put(nums[i],value+1);
+        int xor = 0 ;
+        for(int i=0;i<nums.length ;i++){
+            xor ^= nums[i];
         }
-        for(Map.Entry<Integer,Integer>it: mpp.entrySet()){
-            if(it.getValue()==1){
-                return it.getKey();
-            }
-        }
-        return -1;
+        return  xor;
     }
 }
 
 
 
-//
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int n = nums.length;
+
+//         HashMap<Integer,Integer> mpp = new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             int value = mpp.getOrDefault(nums[i],0);
+//             mpp.put(nums[i],value+1);
+//         }
+//         for(Map.Entry<Integer,Integer>it: mpp.entrySet()){
+//             if(it.getValue()==1){
+//                 return it.getKey();
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+
 // class Solution {
 //     public int singleNumber(int[] nums) {
 //         int n = nums.length;
