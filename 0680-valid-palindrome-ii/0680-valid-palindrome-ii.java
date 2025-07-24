3e8a2216f7114c36a5d53122d6f603e6b67f5e65
Utkarsh Prakash Joshilkar <utkarshjoshilkar@icloud.com>
2025-07-24 19:22:28 +0530

    Time: 4 ms (99.47%), Space: 45.5 MB (41.28%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);   // one time the palindrome is not there another time happens then return false 
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(String s, int left, int right) {
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
