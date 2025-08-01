9f9483a08ba2015f7ff7179781ececf35f853313
Utkarsh Prakash Joshilkar <utkarshjoshilkar@icloud.com>
2025-08-01 08:30:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (27.14%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,40 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][0] <= target && target <= matrix[i][col - 1]) {
-                int low = 0;
-                int high = col - 1;
-
-                while (high >= low) {
-                    int median = low + (high - low) / 2;
-                    if (matrix[i][median] == target) {
-                        return true;
-                    } else if (matrix[i][median] < target) {
-                        low = median + 1;
-                    } else {
-                        high = median - 1;
-                    }
-
-                }
-
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+        int row = 0, col = 0;
+        while (low <= high) {
+            int median = low + (high - low) / 2;
+            row = median / n;   // medians's row 
+            col = median % n;   // medians's col 
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target > matrix[row][col]) {
+                low = median+1;
+            } else {
+                high = median - 1;
             }
         }
         return false;
-
     }
 }
-
-// int median = 0;
-//         int low = matrix[0][0];
-//         int high = matrix[matrix.length - 1][matrix[0].length - 1];
-
-//         while(high>=low){
-//             median = low + high/2;
-//             if(target > median){
-//                 low = median+1;
-//             }
-//         }
-//         return 
